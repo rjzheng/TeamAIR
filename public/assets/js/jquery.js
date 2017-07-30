60d23e26e39fe4a2091872b87f0e8cc42ff9f60b
rjzheng <rjzheng@ucsd.edu>
2017-07-29 18:00:05 -0700

    song set switch

--- public/assets/js/jquery.js
+++ public/assets/js/jquery.js
@@ -1,44 +1,66 @@
+function fetchJson(url, containerGroup) {
+  $.ajax({
+    method: "GET",
+    url: "https://teamair-d90e1.firebaseapp.com" + url})
+    .done(function(config) {
+      createTemplates(config, containerGroup);
+  });
+}
+
+function createTemplates(config, containerGroup) {
+  $.each(config.buttons, function( index ){
+    var btn = config.buttons[index];
+
+    var container = $('<div>');
+    container.addClass('col imgcell');
+
+    var img = $('<img>');
+    img.attr('src', btn.img.src).attr('alt', btn.img.alt);
+
+    var audio = $('<audio>');
+    audio.attr('id', btn.audio.id).attr('controls', 1);
+
+    var source = $('<source>');
+    source.attr('src', btn.audio.src).attr('type', btn.audio.type);
+
+    audio.append(source);
+
+    container.append(img.on({
+      'click': function() {
+        var audio = '#' + btn.audio.id;
+        if ($(audio).prop('paused')) {
+          $(audio).trigger('play');
+        } else {
+          $(audio).trigger('pause');
+        }
+      }
+    }));
+    container.append(audio);
+
+    $(containerGroup).append(container);
+  });
+}
+
 function switch_theme(theme) {
   $('#theme_css').attr('href', theme);
   // trackJs.track('test check');
 }
 
+function switch_beats(config){
+	var disneyContainer  = $("#btn-container-disney");
+	var hipHopContainer = $("#btn-container-hip-hop");
+	if(config === 'hiphop'){
+		disneyContainer.hide();
+		hipHopContainer.show();
+	}
+	else{
+		disneyContainer.show();
+		hipHopContainer.hide();
+	}
+}
+
 $(document).ready(function() {
-  $.ajax({
-    method: "GET",
-    url: "https://teamair-d90e1.firebaseapp.com/assets/config/hipHopConfig.json"})
-    .done(function(hipHopConfig) {
-      $.each(hipHopConfig.buttons, function( index ){
-        var btn = hipHopConfig.buttons[index];
-
-        var container = $('<div>');
-        container.addClass('col imgcell');
-
-        var img = $('<img>');
-        img.attr('src', btn.img.src).attr('alt', btn.img.alt);
-
-        var audio = $('<audio>');
-        audio.attr('id', btn.audio.id).attr('controls', 1);
-
-        var source = $('<source>');
-        source.attr('src', btn.audio.src).attr('type', btn.audio.type);
-
-        audio.append(source);
-
-        container.append(img.on({
-          'click': function() {
-            var audio = '#' + btn.audio.id;
-            if ($(audio).prop('paused')) {
-              $(audio).trigger('play');
-            } else {
-              $(audio).trigger('pause');
-            }
-          }
-        }));
-        container.append(audio);
-
-        $('#btn-container').append(container);
-    });
-
-  });
+  fetchJson('/assets/config/hiphop_config.json', '#btn-container-hip-hop');
+  fetchJson('/assets/config/disney_config.json', '#btn-container-disney');
+  $('#btn-container-disney').hide();
 });
